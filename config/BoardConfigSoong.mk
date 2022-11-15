fc9950ff6454bf1aa97143a580be511d579a9a1f
Pranav Vashi <neobuddy89@gmail.com>
2022-11-15 23:12:29 +0530

    Revert "config: Remove TARGET_CAMERA_BOOTTIME_TIMESTAMP"
    This reverts commit ed8e192818f89f04d6e18c8f97da35e1b166db3f.

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -36,7 +36,8 @@ SOONG_CONFIG_derpGlobalVars += \
     target_ld_shim_libs \
     target_surfaceflinger_udfps_lib \
     uses_oplus_camera \
-    uses_nothing_camera
+    uses_nothing_camera \
+    needs_camera_boottime \
 
 SOONG_CONFIG_NAMESPACES += derpNvidiaVars
 SOONG_CONFIG_derpNvidiaVars += \
@@ -60,6 +61,7 @@ SOONG_CONFIG_derpQcomVars_supports_extended_compress_format := $(AUDIO_FEATURE_E
 SOONG_CONFIG_derpQcomVars_uses_pre_uplink_features_netmgrd := $(TARGET_USES_PRE_UPLINK_FEATURES_NETMGRD)
 SOONG_CONFIG_derpGlobalVars_uses_oplus_camera := $(TARGET_USES_OPLUS_CAMERA)
 SOONG_CONFIG_derpGlobalVars_uses_nothing_camera := $(TARGET_USES_NOTHING_CAMERA)
+SOONG_CONFIG_derpGlobalVars_needs_camera_boottime := $(TARGET_CAMERA_BOOTTIME_TIMESTAMP)
 
 # Set default values
 TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS ?= 0
